579114bb5c4a2ace23f28bdfddce4ccb0d31c1bf
rmdaw00 <64350526+rmdaw00@users.noreply.github.com>
2020-10-13 10:25:05 -0400

    Company Description class, control and view, (no edit view)

---
+++ ContenManagementSystem/Controllers/CompanyDescController.cs
@@ -0,0 +1,24 @@
+﻿using ContenManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContenManagementSystem.Controllers
+{
+    public class CompanyDescController : Controller
+    {
+        // GET: CompanyDesc
+        [ActionName("View")]
+        public ActionResult Index()
+        {
+            CompanyDescClass company = new CompanyDescClass();
+            company.pullData();
+
+            return View(company);
+        }
+
+
+    }
+}
