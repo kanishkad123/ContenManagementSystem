86d3f8e61ab1d8be0500b6ee23f149ddb255a7e6
kanishkad123 <kanishkadeshpande123@gmail.com>
2020-12-13 07:29:43 -0500

    Page Creator

--- ContenManagementSystem/Controllers/PageController.cs
+++ ContenManagementSystem/Controllers/PageController.cs
@@ -32,12 +32,12 @@ namespace ContenManagementSystem.Controllers
             PageManagerView pv = new PageManagerView();
             using (ApplicationDbContext dbContext = new ApplicationDbContext())
             {
-                var pages = dbContext.PageStructures.Select(p => new SelectListItem {Value=p.pageName,Text=p.pageName });
-                pv.pages = new SelectList(pages, "Value", "Text");
-                return View("DynamicPageManager",pv);
+                //var pages = dbContext.PageStructures.Select(p => new SelectListItem {Value=p.pageName,Text=p.pageName });
+                //pv.pages = new SelectList(pages, "Value", "Text");
+                return View("DynamicPageManager");
             }
         }
-
+        [Authorize(Roles= "Admin")]
         public ActionResult PageCreator()
         {
             return View("PageCreator");
@@ -49,10 +49,10 @@ namespace ContenManagementSystem.Controllers
             //string s = markupCreator(pageManagerView);
 
             var doc = new HtmlDocument();
-            doc.LoadHtml(s);
+            //doc.LoadHtml(s);
             var htmlBody = doc.DocumentNode.SelectSingleNode("//div[1]");
-            HtmlNode selectedNode = htmlBody.SelectSingleNode("//div[" + pageManagerView.SectionId + "]/div[" + pageManagerView.DivisionId + "]");
-            selectedNode.AppendChild(HtmlNode.CreateNode("<p>"+pageManagerView.Sectiontext+"</p>"));
+            //HtmlNode selectedNode = htmlBody.SelectSingleNode("//div[" + pageManagerView.SectionId + "]/div[" + pageManagerView.DivisionId + "]");
+            //selectedNode.AppendChild(HtmlNode.CreateNode("<p>"+pageManagerView.Sectiontext+"</p>"));
             ViewData["innerHtml"] = "<div id =\"div1\"><div class='container'>" + htmlBody.OuterHtml + "</div></div>";
             ApplicationDbContext context = new ApplicationDbContext();
             var page = context.Pages.Where(p => p.PageId == "test").FirstOrDefault();
@@ -89,40 +89,100 @@ namespace ContenManagementSystem.Controllers
         public JsonResult temp(Tester model)
         {
             string s = "<div class='container'>";
-            for (int i = 0; i < Convert.ToInt32(model.numberOfSections); i++)
+            for (int i = 0; i < model.sections.Count; i++)
             {
-                s += PageConstants.rows + i;
-                for (int j = 0; j < Convert.ToInt32(model.numberOfDivisions); j++)
+                s += PageConstants.rows;
+                for (int j = 0; j < model.sections[i].divs.Count; j++)
                 {
-                    s += PageConstants.cols[Convert.ToInt32(model.numberOfDivisions)];
+                    s += PageConstants.cols[model.sections[i].divs.Count];
                 }
                 s += "</div>";
             }
             s += "</div>";
 
+            var doc = new HtmlDocument();
+            doc.LoadHtml(s);
+            var htmlBody = doc.DocumentNode.SelectSingleNode("//div[1]");
+            for (int i = 0; i < model.sections.Count; i++)
+            {
+                for (int j = 0; j < model.sections[i].divs.Count; j++)
+                {
+                    int xpath_i = i + 1;
+                    int xpath_j = j + 1;
+                    HtmlNode selectedNode = doc.DocumentNode.SelectSingleNode("//div[1]/div[" + xpath_i + "]/div[" + xpath_j + "]");
+                    switch (model.sections[i].divs[j].divtype)
+                    {
+                        case "text/html":
+                            selectedNode.AppendChild(HtmlNode.CreateNode("<p>" + model.sections[i].divs[j].divData + "</p>"));
+                            break;
+                        case "image":
+                            selectedNode.AppendChild(HtmlNode.CreateNode("<img src='" + model.sections[i].divs[j].divData + "'/>"));
+                            break;
+                        default:
+                            break;
+                    }
+                    
+                }
+            }
+            string htmlFinal = "<div id =\"div1\"><div class='container'>" + htmlBody.OuterHtml + "</div></div>";
+
             using (ApplicationDbContext dbContext = new ApplicationDbContext())
             {
-                dbContext.Pages.Add(new Page {PageId=model.pageName, PageHTML=s });
+                dbContext.Pages.Add(new Page { PageId = model.pageName, PageHTML = htmlFinal });
                 dbContext.SaveChanges();
-                dbContext.PageStructures.Add(new PageStructure
-                {
-                    pageName = model.pageName,
-                    numberOfSections = model.numberOfSections,
-                    numberOfDivisions = model.numberOfDivisions
-                });
-                dbContext.SaveChanges();
+                //dbContext.PageStructures.Add(new PageStructure
+                //{
+                //    pageName = model.pageName,
+                //    numberOfSections = item.sectionNumber,
+                //    numberOfDivisions = item.divisions
+                //});
+                //dbContext.SaveChanges();
             }
             return Json(new { result = "Success" });
         }
+
+        //[HttpPost]
+        //public JsonResult pageSections(string pageName)
+        //{
+        //    string pageSec = "";
+        //    using (ApplicationDbContext dbContext = new ApplicationDbContext())
+        //    {
+        //        pageSec = dbContext.PageStructures.Where(p => p.pageName == pageName)
+        //            .OrderBy(p=>p.numberOfSections+" "+ "descending").FirstOrDefault().numberOfSections;
+        //    }
+        //    return Json(new { sections = pageSec });
+        //}
+
+        //[HttpPost]
+        //public JsonResult pageDivisions(string pageSectionNumber)
+        //{
+        //    string div = "";
+        //    using (ApplicationDbContext dbContext = new ApplicationDbContext())
+        //    {
+        //        div = dbContext.PageStructures.Where(p => p.pageName == pageSectionNumber.Split('&')[0] && p.numberOfSections == pageSectionNumber.Split('&')[1])
+        //            .FirstOrDefault().numberOfDivisions;
+        //    }
+        //    return Json(new { divisions = div });
+        //}
+
     }
 
     public class Tester
     {
         public string pageName { get; set; }
-        public string numberOfSections { get; set; }
+        public List<tem> sections { get; set; }
+    }
+
+    public class tem
+    {
         public string sectionNumber { get; set; }
-        public string numberOfDivisions { get; set; }
-        public string divisionNumber { get; set; }
-        public string divisionType { get; set; }
+        public List<tt> divs { get; set; }
+    }
+
+    public class tt
+    {
+        public string divnumber { get; set; }
+        public string divtype { get; set; }
+        public string divData { get; set; }
     }
 }
