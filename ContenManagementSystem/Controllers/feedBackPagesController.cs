c9752e4a79a04fc2468251369f3ae07d39bfb539
suvaanshkumar <34331549+suvaanshkumar@users.noreply.github.com>
2020-12-13 15:26:11 -0500

    emailWorking

--- ContenManagementSystem/Controllers/feedBackPagesController.cs
+++ ContenManagementSystem/Controllers/feedBackPagesController.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using ContenManagementSystem.DAL;
@@ -114,6 +115,48 @@ namespace ContenManagementSystem
             db.feedBackPages.Remove(feedBackPage);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendEmail(string receiver, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("testeraspt@gmail.com", "testeraspt");
+                    var receiverEmail = new MailAddress(receiver, "suvaansh");
+                    var password = "test@1234";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
         }
 
         protected override void Dispose(bool disposing)
